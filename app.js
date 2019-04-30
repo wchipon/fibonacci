ee9a28d201d3cffdfd6163345bb95876cca61c55
wchipon <uchpon115@yahoo.co.jp>
2019-04-30 04:59:49 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
 function fib(n) {
-    if (memo.has(n)) {
+    if(memo.has(n)) {
         return memo.get(n);
     }
     const value = fib(n - 1) + fib(n - 2);
